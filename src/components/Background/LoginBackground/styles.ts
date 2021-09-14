f17f28aa6d49dab5157484659d598e97894007db
wotjd1799 <wotjd0676@gmail.com>
2021-09-14 23:42:20 +0900

    ⚡️ :: Login Background
    로그인 배경 작업

---
+++ src/components/Background/LoginBackground/styles.ts
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+import { leftGroup, rightHalf, rightPolygon } from '../../../assets/index'
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+
+export const LeftGroup = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%) scale(0.9);
+  width: 841px;
+  height: 965px;
+  background-image: url(${leftGroup});
+`
+
+export const RightHalf = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 5vw;
+  transform-origin: bottom;
+  transform: scale(0.9);
+  width: 302px;
+  height: 195px;
+  background-image: url(${rightHalf});
+`
+
+export const RightPolygon = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  transform: scale(0.9);
+  width: 434px;
+  height: 434px;
+  background-image: url(${rightPolygon});
+  `
