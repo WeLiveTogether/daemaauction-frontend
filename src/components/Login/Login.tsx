51b08b1d39285fde58b3435601d7152582e3b962
LeeJaeSeoung <wotjd0676@gmail.com>
2021-11-09 19:20:21 +0900

    ⚡️ :: Login button
    로그인 버튼 수정

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -5,13 +5,7 @@ const Login = ():JSX.Element => {
     <S.Container>
       <S.GridContainer>
         <S.LoginContainer>
-          <S.Title>로그인</S.Title>
-          <S.GuideText>아이디</S.GuideText>
-          <S.InputID />
-          <S.GuideText>비밀번호</S.GuideText>
-          <S.InputPassword type="password"/>
-          <S.LoginButton>로그인</S.LoginButton>
-          <S.Semicolone>with Semicolone;</S.Semicolone>
+          <S.LoginButton>구글로 로그인</S.LoginButton>
         </S.LoginContainer>
       </S.GridContainer>
     </S.Container>
