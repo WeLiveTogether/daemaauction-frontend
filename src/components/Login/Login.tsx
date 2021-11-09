b6b9bd39c2059bd69ed2ee9d8f3cd435c3e901b6
LeeJaeSeoung <wotjd0676@gmail.com>
2021-11-09 20:20:34 +0900

    ⚡️ :: Add various login methods
    다양한 로그인 방식을 위한 UI 개선

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,45 @@
+import { color } from "../../styles/color";
 import * as S from "./styles";
 
 const Login = (): JSX.Element => {
+  interface Button {
+    text: string;
+    color: string;
+    // onclick: () => void;
+  }
+
+  const buttons: Button[] = [
+    {
+      text: "네이버 로그인",
+      color: color.green,
+      // onclick: naver(),
+    },
+    {
+      text: "카카오 로그인",
+      color: color.yellow,
+      // onclick: kakao(),
+    },
+    {
+      text: "페이스북 로그인",
+      color: color.blue,
+      // onclick: facebook(),
+    },
+    {
+      text: "구글 로그인",
+      color: color.gray,
+      // onclick: google(),
+    },
+  ];
+
   return (
     <S.Container>
       <S.GridContainer>
-        <S.LoginButton>구글로 로그인</S.LoginButton>
+        <S.LoginContainer>
+          {buttons.map((value) => {
+            const { color, text } = value;
+            return <S.LoginButton color={color}>{text}</S.LoginButton>;
+          })}
+        </S.LoginContainer>
       </S.GridContainer>
     </S.Container>
   );
