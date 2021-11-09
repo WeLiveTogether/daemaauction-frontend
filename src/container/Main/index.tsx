64112ab3166a454b4b1d05140766d59d9030b7b9
LeeJinWoo <wlsdn5793@gmail.com>
2021-11-09 20:24:29 +0900

    🐛:: conflict

--- src/container/Main/index.tsx
+++ src/container/Main/index.tsx
@@ -1,11 +1,10 @@
 import Background from "../../components/Background/MainBackground/MainBackground";
-import Header from "../../components/Header/Header";
+
 const MainContainer = (): JSX.Element => {
   return (
-    <div>
-      <Header />
+    <>
       <Background />
-    </div>
+    </>
   );
 };
 
--- src/router/mainRouter.tsx
+++ src/router/mainRouter.tsx
@@ -2,6 +2,7 @@ import React, { FC, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
 const RegisterContainer = React.lazy(() => import("../container/Register"));
 const MainContainer = React.lazy(() => import("../container/Main"));
+
 const MainRouter: FC = (): JSX.Element => {
   return (
     <Suspense fallback={<div>로딩중...</div>}>
