326636ea579e0d9ce176205a934510803e5f31f3
LeeJaeSeoung <wotjd0676@gmail.com>
2021-11-13 21:04:06 +0900

    ⚡️ :: Integrated Background
    유동적인 백그라운드

--- src/components/Background/MainBackground/MainBackground.tsx
+++ src/components/Background/MainBackground/MainBackground.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+import Header from "../../Header/Header";
 import * as S from "./styles";
 
 type PropType = {
@@ -5,15 +7,38 @@ type PropType = {
 };
 
 const Background = ({ children }: PropType): JSX.Element => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const backgroundContainerRef = useRef<HTMLDivElement>(null);
+
+  const getHeight = () => {
+    const { current: container } = containerRef;
+    const { current: backgroundContainer } = backgroundContainerRef;
+
+    if (container && backgroundContainer) {
+      backgroundContainer.style.height = `${document.body.clientHeight}px`;
+    }
+    
+    return document.body.clientHeight;
+  }
+  
+  window.onload = getHeight;
+
+  useEffect(() => {
+    getHeight();
+  }, [children]);
+
   return (
     <>
-      <S.LeftTopYellow />
-      <S.LeftTopGreen />
-      <S.RightTopGreen />
-      <S.RightTopYellow />
-      <S.GreenPolygon />
-      <S.YellowHalf />
-      <S.Container>
+      <Header />
+      <S.BackgroundContainer ref={backgroundContainerRef}>
+        <S.LeftTopYellow />
+        <S.LeftTopGreen />
+        <S.RightTopGreen />
+        <S.RightTopYellow />
+        <S.GreenPolygon />
+        <S.YellowHalf />
+      </S.BackgroundContainer>
+      <S.Container ref={containerRef}>
         <S.GridContainer>{children}</S.GridContainer>
       </S.Container>
     </>
