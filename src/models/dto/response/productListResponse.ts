1f1e90752324e718e4e97bb038e594431d4a7525
김진근 <freedom7113@gmail.com>
2021-11-16 22:25:19 +0900

    ⚡️ :: complete get Popular Products api

--- src/models/dto/response/productListResponse.ts
+++ src/models/dto/response/productListResponse.ts
@@ -1,1 +1,14 @@
-export default interface productListResponse {}
+export interface Product {
+  auctionPrice: number;
+  content: string;
+  createDate: string;
+  imageUrl: string;
+  immePrice: number;
+  productId: number;
+  title: string;
+  views: number;
+}
+
+type productListResponse = Product[];
+
+export default productListResponse;
