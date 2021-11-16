a06c7fb6224e4636afaabada41af2b8ed658b652
김진근 <freedom7113@gmail.com>
2021-11-16 22:25:19 +0900

    ⚡️ :: complete get Popular Products api

--- src/utils/api/Main/index.ts
+++ src/utils/api/Main/index.ts
@@ -1,9 +1,12 @@
 import uri from "../../../constance/uri";
+import productListResponse from "../../../models/dto/response/productListResponse";
 import { DEAMA_AUCTION, getRequest } from "../../axios";
 
 export const getPopularProducts = async () => {
   const request = getRequest(DEAMA_AUCTION);
-  const response = await request.get(uri.productPopularity);
-
-  console.log(response);
+  try {
+    return await request.get<productListResponse>(uri.productPopularity);
+  } catch (error) {
+    return Promise.reject(error);
+  }
 };
