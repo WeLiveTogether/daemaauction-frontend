8d158ac7632c26071fe3c3a60c70dd50cb00505f
김진근 <freedom7113@gmail.com>
2021-11-16 22:24:09 +0900

    ⚡️ :: move style

---
+++ src/components/ProductCard/styles.ts
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled";
+
+export const HotItem = styled.div`
+  margin-bottom: 30px;
+`;
+
+export const ItemImg = styled.div`
+  width: 100%;
+  margin-bottom: 12px;
+`;
+
+export const ItemTitle = styled.div`
+  width: 100%;
+  margin-bottom: 8px;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const ItemDetailTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 16px;
+  font-weight: 400;
+  color: #8395a7;
+  margin-bottom: 8px;
+`;
+
+export const ItemDetailBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 16px;
+  font-weight: medium;
+`;
+
+export const ItemPrice = styled.div`
+  color: #1cd0a1;
+`;
+
+export const CountImg = styled.img`
+  margin-right: 8px;
+`;
+
+export const Count = styled.div`
+  color: #212f3e;
+`;
