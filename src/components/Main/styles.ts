b7cd5f107b2de6897a64dd4de2f976e1487ba70a
김진근 <freedom7113@gmail.com>
2021-11-17 21:47:25 +0900

    ⚡️ :: write style

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import { color } from "../../styles/color";
 
 export const Container = styled.div`
   margin-top: 60px;
@@ -25,6 +27,11 @@ export const SmallTitle = styled.div`
   font-size: 24px;
 `;
 
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: ${color.black};
+`;
+
 export const More = styled.div`
   font-weight: medium;
   font-size: 16px;
@@ -34,4 +41,5 @@ export const HotItemList = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   column-gap: 30px;
+  row-gap: 30px;
 `;
