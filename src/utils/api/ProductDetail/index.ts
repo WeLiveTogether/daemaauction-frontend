3928c38e80ab7f8273600b1e41fdd36fb0547438
김진근 <freedom7113@gmail.com>
2021-11-18 10:07:37 +0900

    ⚡️ :: use response

--- src/utils/api/ProductDetail/index.ts
+++ src/utils/api/ProductDetail/index.ts
@@ -1,10 +1,11 @@
 import uri from "../../../constance/uri";
 import { DEAMA_AUCTION, getRequest } from "../../axios";
+import productDetailResponse from "../../../models/dto/response/productDetailResponse";
 
 export const getProductDetail = async (id: number) => {
   const request = getRequest(DEAMA_AUCTION);
   try {
-    return await request.get(`${uri.productDetails}/${id}`);
+    return await request.get<productDetailResponse>(`${uri.productDetails}/${id}`);
   } catch (error) {
     return Promise.reject(error);
   }
