4c1d860f5b136fca3b9a1309f7feb29dc56b7b0b
김진근 <freedom7113@gmail.com>
2021-11-21 23:35:41 +0900

    ⚡️ :: 스타일 변경

--- src/components/Category/styled.ts
+++ src/components/Category/styled.ts
@@ -24,7 +24,7 @@ export const Category = styled.div`
 `;
 
 export const CategoryBox = styled.div`
-  width: 141px;
+  /* max: 200px; */
   display: flex;
   justify-content: space-between;
   cursor: pointer;
@@ -32,34 +32,11 @@ export const CategoryBox = styled.div`
 
 export const CategoryRight = styled.div`
   display: flex;
-`
+`;
 
 export const Chevron = styled.img<{ rotate: number }>`
   transform: rotate(${(props) => props.rotate}deg);
   transition: 0.3s;
-`;
-
-export const InnerItemList = styled.div<{ display: string }>`
-  margin-top: 6px;
-  display: ${(props) => props.display};
-  color: ${color.black};
-`;
-
-export const InnerItem = styled.div`
-  padding: 4px 8px;
-  background-color: ${color.white};
-  :hover {
-    color: ${color.white};
-    background-color: ${color.green};
-  }
-  :first-child {
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-  }
-  :last-child {
-    border-bottom-left-radius: 10px;
-    border-bottom-right-radius: 10px;
-  }
 `;
 
 export const ItemContainer = styled.div`
