091e1597e9bca2ce33d3155feb55773dbbd19d92
김진근 <freedom7113@gmail.com>
2021-11-22 01:25:16 +0900

    ⚡️ :: api 연동

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,8 +1,15 @@
 import { useEffect, useLayoutEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { Select } from "../../assets";
+import { getBigCategory, getSubCategory } from "../../utils/api/Category";
 import DropDown from "./DropDown/DropDown";
+import { Product } from "../../models/dto/response/productListResponse";
 import * as S from "./styled";
+import { AxiosResponse } from "axios";
+import categoryResponse from "../../models/dto/response/categoryResponse";
+import ProductCard from "../ProductCard/ProductCard";
+import { getSuggestedQuery } from "@testing-library/dom";
+import ProductCardSkeleton from "../ProductCardSkeleton/ProductCardSkeleton";
 interface PropsType {
   bigCategoryParam: string | null;
   subCategoryParam: string | null;
@@ -38,6 +45,7 @@ const Category = ({ bigCategoryParam, subCategoryParam }: PropsType) => {
   const [isSubActive, setIsSubActive] = useState<boolean>(false);
   const [bigIndex, setBigIndex] = useState<number>(initBig());
   const [subIndex, setSubIndex] = useState<number>(initSub());
+  const [products, setProducts] = useState<Product[] | null>(null);
 
   const { push } = useHistory();
 
@@ -49,7 +57,39 @@ const Category = ({ bigCategoryParam, subCategoryParam }: PropsType) => {
       setBigIndex(0);
       setSubIndex(0);
     }
+    setProducts(null);
+    settingProducts();
   }, [bigCategoryParam, subCategoryParam]);
+
+  const settingProducts = async () => {
+    try {
+      const request = subIndex === 0 ? getBigCategory : getSubCategory;
+      const category = subIndex === 0 ? bigCategoryParam! : subCategoryParam!;
+      const response = await request(category);
+      setProducts(
+        response.data.map((value) => {
+          const { auctionPrice, categoryId, id, name, user, views } = value;
+          const data: Product = {
+            title: name,
+            imageUrl: "",
+            views: views,
+            auctionPrice: auctionPrice,
+            userName: user.username,
+            category: categoryId.name,
+            productId: id,
+            content: "",
+            createDate: "",
+            immePrice: 0,
+            subCategory: "",
+          };
+
+          return data;
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useLayoutEffect(() => {
     const subList = subCategorys.get(bigCategorys[bigIndex]);
@@ -126,7 +166,15 @@ const Category = ({ bigCategoryParam, subCategoryParam }: PropsType) => {
         </S.Category>
       </S.CategoryContainer>
 
-      <S.ItemContainer></S.ItemContainer>
+      <S.ItemContainer>
+        {products
+          ? products.map((value) => {
+              return <ProductCard product={value} />;
+            })
+          : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => {
+              return <ProductCardSkeleton />;
+            })}
+      </S.ItemContainer>
     </S.Container>
   );
 };
