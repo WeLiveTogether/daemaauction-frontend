2aab5ebb2a94cc5e06734a3585782c0c7be20a3a
김진근 <freedom7113@gmail.com>
2021-11-22 00:00:27 +0900

    ⚡️ :: 파라미터 가져오기

--- src/container/Category/index.tsx
+++ src/container/Category/index.tsx
@@ -1,13 +1,17 @@
 import Category from "../../components/Category/Category";
 import Background from "../../components/Background/MainBackground/MainBackground";
+import { useLocation } from "react-router";
 
 const CategoryContainer = (): JSX.Element => {
+  const query = new URLSearchParams(useLocation().search);
   return (
     <>
       <Background>
-        <Category />
+        <Category
+          bigCategoryParam={query.get("category")}
+          subCategoryParam={query.get("subcategory")}
+        />
       </Background>
-      
     </>
   );
 };
