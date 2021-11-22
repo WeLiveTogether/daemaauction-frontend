4233e913985e032c36ba246c7b8c1bc703bb5f88
LeeJaeSeoung <wotjd0676@gmail.com>
2021-11-22 23:39:05 +0900

    :bug: :: Bug Fix
    도메인 오류 또 나서 수정

--- src/utils/api/Mypage/index.ts
+++ src/utils/api/Mypage/index.ts
@@ -1,9 +1,9 @@
 import uri from "../../../constance/uri";
 import { MyPageProductList } from "../../../models/dto/response/productListResponse";
-import { getRequest, DEAMA_AUCTION } from "../../axios";
+import { getRequest, DAEMA_AUCTION } from "../../axios";
 
 export const getUserInfo = async () => {
-  const request = getRequest(DEAMA_AUCTION);
+  const request = getRequest(DAEMA_AUCTION);
   try {
     return await request.get(uri.users);
   } catch (error) {
@@ -12,7 +12,7 @@ export const getUserInfo = async () => {
 };
 
 export const getMyProduct = async () => {
-  const request = getRequest(DEAMA_AUCTION);
+  const request = getRequest(DAEMA_AUCTION);
   try {
     return await request.get<MyPageProductList>(uri.myProduct);
   } catch(error) {
